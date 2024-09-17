7a1b1d80e0dea1ce7373a98bce2f4570ac1e1e95
Roman Chaban <roman.chaban1001@gmail.com>
2024-09-17 01:06:37 -0800

    Initialize Next.js project with default template

---
+++ src/components/common/DashboardSidebar/DashboardSidebar .tsx
@@ -0,0 +1,7 @@
+import type { FC } from 'react';
+
+interface DashboardSidebarProps {}
+
+export const DashboardSidebar: FC<DashboardSidebarProps> = () => {
+  return <aside></aside>;
+};
---
+++ src/fonts/fonts.ts
@@ -0,0 +1,16 @@
+import { Inter, Roboto, Lato } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+export const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '500', '700', '900'],
+});
+
+export const lato = Lato({
+  subsets: ['latin'],
+  weight: ['400', '700', '900'],
+});
